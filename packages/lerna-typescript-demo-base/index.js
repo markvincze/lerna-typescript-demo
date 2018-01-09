1f1feb1db5d06d954c5f432cb78ba60da2435792
Mark Vincze <mrk.vincze@gmail.com>
2018-01-09 20:54:35 +0100

    Fix BehaviorSubject import

--- packages/lerna-typescript-demo-base/index.js
+++ packages/lerna-typescript-demo-base/index.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var BehaviorSubject = require('rxjs/BehaviorSubject');
+var { BehaviorSubject } = require('rxjs/BehaviorSubject');
 
 function Foo() {
 }
